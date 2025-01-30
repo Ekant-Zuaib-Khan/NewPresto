87dd31ba5a88e21b59a479139c32c4cda548db82
Ekant-Zuaib-Khan <zuaib.khan@ekant.in>
2025-01-30 18:14:01 +0530

    design updates

--- newpresto/src/app/page.tsx
+++ newpresto/src/app/page.tsx
@@ -10,10 +10,22 @@ import PlasticPlaCat from "../assets/images/plasticPart.jpg";
 import FirstLogo from "../assets/images/msme.svg";
 import SecLogo from "../assets/images/nabl.svg";
 import ThirdLogo from "../assets/images/zed_gold.svg";
+import whyImg from "../assets/images/high.jpg";
+import WhyCard from "@/components/Cards/WhyCard";
+import CostIcon from "../assets/images/cost.jpg";
+import ExpertiseIcon from "../assets/images/experties.jpg";
+import HighlyIcon from "../assets/images/team.jpg"
 
 interface Category {
   image: StaticImageData | string;
   title: string;
+}
+
+interface WhyCardData {
+  imageSrc: StaticImageData | string;
+  altText: string;
+  heading: string;
+  description: string;
 }
 
 const categories: Category[] = [
@@ -23,6 +35,34 @@ const categories: Category[] = [
   { image: EnvCat, title: "Plastic & Polymer Testing Instruments" },
   { image: BotCat, title: "Plastic & Polymer Testing Instruments" },
   { image: PlasticPlaCat, title: "Plastic & Polymer Testing Instruments" },
+];
+
+const whyCardData: WhyCardData[] = [
+  {
+    imageSrc: whyImg,
+    altText: "High Quality",
+    heading: "High Quality & Perfection",
+    description: "We are National Quality Award winners from the Government of India. All our products are unmatched in quality and design.",
+  },
+  {
+    imageSrc: CostIcon,
+    altText: "High Quality 2",
+    heading: "Cost Benefit",
+    description: "All our Products are manufactured in our 2 state of the art factories. They are standardised using CNC machines to provide long term cost benifits to customers.",
+  },
+  {
+    imageSrc: ExpertiseIcon,
+    altText: "Expertise & Adaptability",
+    heading: "Expertise & Adaptability",
+    description: "40+ years of experience in providing testing solutions to the world.",
+  },
+
+  {
+    imageSrc: HighlyIcon,
+    altText: "Expertise & Adaptability",
+    heading: "Highly Motivated Team",
+    description: "180+ Team members to provide best services PAN India and globally.",
+  },
 ];
 
 export default function Home() {
@@ -72,7 +112,8 @@ export default function Home() {
             <div className="col-span-5 grid grid-cols-2 gap-10 ">
               <div className="flex flex-col gap-4">
                 <h2 className="text-2xl">
-                  Please take a moment to fill <br /> in the details and our team shall <br />
+                  Please take a moment to fill <br /> in the details and our
+                  team shall <br />
                   get in touch with you.
                 </h2>
 
@@ -82,23 +123,90 @@ export default function Home() {
 
                 <div className="w-28 h-1 bg-primaryColor rounded-full" />
                 <div className="grid grid-cols-3 gap-3">
-                  <Image src={FirstLogo} alt="firstLogo" loading="lazy" className="border rounded-md p-1 border-black" />
-                  <Image src={SecLogo} alt="firstLogo" loading="lazy" className="border rounded-md p-1 border-black" />
-                  <Image src={ThirdLogo} alt="firstLogo" loading="lazy" className="border rounded-md p-1 border-black" />
+                  <Image
+                    src={FirstLogo}
+                    alt="firstLogo"
+                    loading="lazy"
+                    className="border rounded-md p-1 border-black"
+                  />
+                  <Image
+                    src={SecLogo}
+                    alt="firstLogo"
+                    loading="lazy"
+                    className="border rounded-md p-1 border-black"
+                  />
+                  <Image
+                    src={ThirdLogo}
+                    alt="firstLogo"
+                    loading="lazy"
+                    className="border rounded-md p-1 border-black"
+                  />
                 </div>
               </div>
 
               <div className="border border-gray-300 bg-white p-5">
-                <h4 className="text-xl text-center mb-3" >Contact Us</h4>
-                <input type="text" placeholder="Name" className="contactInput"/>
-                <input type="text" placeholder="Name" className="contactInput"/>
-                <input type="text" placeholder="Name" className="contactInput"/>
-                <textarea rows={3} placeholder="Name" className="contactInput !mb-1"/>
-                <button className="bg-primaryColor text-white w-full rounded-md py-2">submit</button>
+                <h4 className="text-xl text-center mb-3">Contact Us</h4>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="contactInput"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="contactInput"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="contactInput"
+                />
+                <textarea
+                  rows={3}
+                  placeholder="Name"
+                  className="contactInput !mb-1"
+                />
+                <button className="bg-primaryColor text-white w-full rounded-md py-2">
+                  submit
+                </button>
               </div>
             </div>
-            <div  />
+            <div />
           </div>
+        </div>
+      </section>
+ 
+
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h1 className="text-4xl font-bold mb-3">Why Presto ?</h1>
+          <p >
+            Because over 11,436 customers across the globe have put their faith
+            in us and we have surpassed their expectations. And, because we
+            ensure...
+          </p>
+
+          <div className="grid grid-cols-7 gap-5 mt-16">
+            <div />
+            <div className="col-span-5 grid grid-cols-2 gap-10 ">
+            {whyCardData.map((item, index) => (
+                <WhyCard
+                  key={index}
+                  imageSrc={item.imageSrc}
+                  altText={item.altText}
+                  heading={item.heading}
+                  description={item.description}
+                />
+              ))}
+            </div>
+            <div />
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-tertoryColor text-center text-xl">Trusted by the brands you trust</h3>
         </div>
       </section>
     </>
---
+++ newpresto/src/components/Cards/WhyCard.tsx
@@ -0,0 +1,31 @@
+// components/HighQualityCard.tsx
+import Image from "next/image";
+import React from "react";
+
+interface HighQualityCardProps {
+  imageSrc: string;
+  altText: string;
+  heading: string;
+  description: string;
+}
+
+const WhyCard: React.FC<HighQualityCardProps> = ({ imageSrc, altText, heading, description }) => {
+  return (
+    <div className="flex gap-3 mb-5">
+      <div className="w-30 h-30 rounded-full">
+        <Image
+          src={imageSrc}
+          alt={altText}
+          priority={true}
+          className="w-full rounded-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h4 className="text-xl mb-2">{heading}</h4>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WhyCard;
